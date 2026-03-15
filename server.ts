ad2a49e1c6b724c9887eb0c3e00954c8a62c5660
konxu <41413929+konxu@users.noreply.github.com>
2026-03-15 15:15:18 +1100

    Update server.ts

--- server.ts
+++ server.ts
@@ -27,16 +27,12 @@ app.listen(Number(PORT), "0.0.0.0", () => {
     });
     app.use(vite.middlewares);
   } else {
-    const path = await import("path");
-  const distPath = path.join(process.cwd(), "dist");
-  app.use(express.static(distPath));
-  app.get("*", (req, res) => {
-      res.sendFile(path.join(distPath, "index.html"), (err) => {
-        if (err) {
-          console.error("发送 index.html 失败:", err);
-          res.status(500).send("服务器内部错误：找不到前端入口文件");
-        }
-      });
+    const distPath = path.join(process.cwd(), "dist");
+    app.use(express.static(distPath));
+    
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(distPath, "index.html"));
     });
 }
 
