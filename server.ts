6a971a38e5abce9b3e33b6825bc87b89a8bed433
konxu <41413929+konxu@users.noreply.github.com>
2026-03-15 14:34:24 +1100

    Update server.ts

--- server.ts
+++ server.ts
@@ -26,8 +26,11 @@ app.listen(Number(PORT), "0.0.0.0", () => {
     });
     app.use(vite.middlewares);
   } else {
-    app.use(express.static("dist"));
-  }
+    const path = await import("path");
+  const distPath = path.join(process.cwd(), "dist");
+  app.use(express.static(distPath));
+  app.get("*", (req, res) => res.sendFile(path.join(distPath, "index.html")));
+}
 
   app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${PORT}`);
