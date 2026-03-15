7d2dd13afac1733e659e43106b246dec3ead4110
konxu <41413929+konxu@users.noreply.github.com>
2026-03-15 14:12:20 +1100

    Update server.ts

--- server.ts
+++ server.ts
@@ -6,7 +6,10 @@ dotenv.config();
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000; 
+app.listen(Number(PORT), "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
   app.use(express.json());
 
